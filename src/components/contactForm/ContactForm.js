a95ecb684ed079f69c981e441e31999840af9834
AlexisWeiss <alexissxo143@gmail.com>
2023-01-05 11:37:38 -0800

    add contact form

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -10,6 +10,29 @@ export const ContactForm = ({
   handleSubmit
 }) => {
   return (
-    ContactForm
+    <form onSubmit={handleSubmit}>
+      <input type='text'
+              name="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              placeholder='Contact Name'
+              required/>
+      <input type='tel'
+             name='phone'
+             value={phone}
+             onChange={(e) => setPhone(e.target.value)}
+             pattern="[1-9][0-9]{2}-[1-9][0-9]{2}-[0-9]{4}"
+             placeholder="Contact Phone (###-###-####)"
+             required/>
+      <input type='email'
+            name='email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder='Contact Email'
+            required/>
+      <input type='submit'
+              value='Add Contact'/>
+
+    </form>
   );
 };
